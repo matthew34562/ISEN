d328af015c89d276ab280d715a733c1b055c9028
Matthew <90023292@study.curtincollege.edu.au>
2021-07-31 03:44:58 -0700

    Replaced ifs

--- Maze.py
+++ Maze.py
@@ -65,20 +65,18 @@ if __name__ == "__main__":
             print("You fall into the chasm of doom.") # Out of bounds.
 
         else:
-            if grid[currentRow][currentCol] == EMPTY:
-                grid[currentRow][currentCol] = VISITED
-
-            elif grid[currentRow][currentCol] == WALL:
-                done = True
-                print("You stumble blindly into a solid concrete wall.") # Hit wall.
-
-            elif grid[currentRow][currentCol] == END:
-                done = True
-                solved = True
-                print("SOLVED!") # Solved.
-
+            cell = grid[currentRow][currentCol]
+            if cell == EMPTY:
+                    grid[currentRow][currentCol] = VISITED
+            elif cell == WALL:
+                    done = True
+                        print("You stumble blindly into a solid concrete wall.")
+            elif cell == END:
+                    done = True
+                        solved = True
+                            print("SOLVED!")
             else:
-                pass # Do nothing
+                    pass # Do nothing
 
         charIndex += 1
     # end-while
